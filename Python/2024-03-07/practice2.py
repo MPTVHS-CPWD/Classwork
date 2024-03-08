9b3e227e1838b8122cc30c95e15023e892a2cad9
Winders School <warias15@bostonk12.org>
2024-03-08 09:15:22 -0500

    practice2.py 2024-03-07

---
+++ Python/2024-03-07/practice2.py
@@ -0,0 +1,12 @@
+# Winders Arias
+# 04/07/2024
+
+name = input("Please enter your name: ")
+fav_food = input("Please enter your favorite food: ")
+fav_song = input("Please enter your favorite song: ")
+fav_quote = input("Please enter your favorite quote: ")
+print(f'\nInformation about {name}:\n*****\nFavorite food: {fav_food}\n'
+      f'Favorite song: "{fav_song}"\n'
+      f'Favorite quote: "{fav_quote}"\n\n'
+      f"Summary: {name}'s favorite food is {fav_food}, their favorite song is \"{fav_song}\" "
+      f"and their favorite quote is \"{fav_quote}\".")
