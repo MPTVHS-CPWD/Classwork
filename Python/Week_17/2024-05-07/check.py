1d9e2bd4d8ee8132dfd9ca548c85fdb318ced3ee
Winders School <warias15@bostonk12.org>
2024-05-09 07:59:59 -0400

    Added week 17 assignments

---
+++ Python/Week_17/2024-05-07/check.py
@@ -0,0 +1,21 @@
+# Winders Arias
+# 5/7/2024
+
+# Creating an empty list
+a = []
+
+# Using the operator to add to the list
+a += [1]
+a += [2]
+a += [3]
+
+# Printing the list
+print(a)
+
+# Creating an emtpy list named b
+b = []
+
+# Using the function to add to the list b
+b.append(1)
+b.append(2)
+b.append(3)
---
+++ Python/Week_17/2024-05-08/lists.py
@@ -0,0 +1,12 @@
+# Winders Arias
+# 5/8/2024
+
+# Creating an empty list
+lst = []
+
+# Using a for loop to add the numbers 1 through 100 to the list
+for i in range(1, 101):
+    lst.append(i)
+
+# Printing the lst
+print(lst)
