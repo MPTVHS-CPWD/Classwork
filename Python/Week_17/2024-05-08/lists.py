9c1c48dbe6a2ef0615755b5defcbd69437d2c8e2
Winders School <warias15@bostonk12.org>
2024-05-09 10:43:11 -0400

    Added week 17 lists.py

--- Python/Week_17/2024-05-08/lists.py
+++ Python/Week_17/2024-05-08/lists.py
@@ -8,5 +8,31 @@ lst = []
 for i in range(1, 101):
     lst.append(i)
 
-# Printing the lst
+# Printing the list lst
 print(lst)
+
+# Creating an empty list named odd
+odd = []
+
+# Using a for loop to add the odd numbers 1 through 100 to the list odd
+for i in range(1, 101, 2):
+    odd.append(i)
+
+# Printing the list odd
+print(odd)
+
+# Creating an empty list named even
+even = []
+
+# Using a for loop to add the even numbers 1 through 100 to the list even
+for i in range(2, 101, 2):
+    even.append(i)
+
+# Printing the list even
+print(even)
+
+# Create a variable named "b" that points to the first list that you created
+b = lst
+
+# Create a variable named joined that joins the even and odd lists using an operator
+joined = odd + even
